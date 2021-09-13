0186ce1dee5c5b7070247a1237c812dfef65fdce
Dev-IV-Dnl <mattgiordanengo@gmail.com>
2021-09-13 11:10:43 +0200

    first commit

---
+++ js/PostIt.js
@@ -0,0 +1,17 @@
+class PostIt {
+    x;
+    y;
+    longueur;
+    hauteur;
+    couleur;
+    texte;
+
+    constructor(x, y, longueur, hauteur, couleur, texte) {
+        this.x = x;
+        this.y = y;
+        this.longueur = longueur;
+        this.hauteur = hauteur;
+        this.couleur = couleur;
+        this.texte = texte;
+    }
+}
