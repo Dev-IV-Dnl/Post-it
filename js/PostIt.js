aa9fe8c779169560a7bfa13552f412a588328994
Dev-IV-Dnl <mattgiordanengo@gmail.com>
2021-09-14 09:37:12 +0200

    maj 13 septembre

--- js/PostIt.js
+++ js/PostIt.js
@@ -1,4 +1,5 @@
 class PostIt {
+    num;
     x;
     y;
     longueur;
@@ -6,12 +7,64 @@ class PostIt {
     couleur;
     texte;
 
-    constructor(x, y, longueur, hauteur, couleur, texte) {
+    constructor(num, x, y, couleur, texte, longueur = 150, hauteur = 150) {
+        this.num = num;
         this.x = x;
         this.y = y;
-        this.longueur = longueur;
-        this.hauteur = hauteur;
         this.couleur = couleur;
         this.texte = texte;
+        this.longueur = longueur;
+        this.hauteur = hauteur;
+    }
+
+    deplace(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    agrandi(longueur, hauteur) {
+        this.longueur = longueur;
+        this.hauteur = hauteur;
+    }
+
+    edit(texte) {
+        this.texte = texte;
+    }
+
+    affiche() {
+        let monPostIt = document.createElement('div');
+        monPostIt.classList.add("postIt");
+        monPostIt.id = "PostIt" + this.num;
+        conteneur.appendChild(monPostIt);
+        monPostIt.style.marginTop = this.x + "px";
+        monPostIt.style.marginLeft = this.y + "px";
+        monPostIt.style.width = this.longueur + "px";
+        monPostIt.style.height = this.hauteur + "px";
+        monPostIt.style.backgroundColor = this.couleur;
+        monPostIt.textContent = this.texte;
+
+        let tousLesBoutons = document.createElement('div');
+        tousLesBoutons.classList.add("tousLesBoutons");
+        monPostIt.appendChild(tousLesBoutons);
+
+        let btnDeplace = document.createElement('button');
+        btnDeplace.classList.add("btnDeplace");
+        tousLesBoutons.appendChild(btnDeplace);
+        btnDeplace.innerHTML = "<i class='fas fa-arrows-alt'></i>";
+
+        let btnAgrandi = document.createElement('button');
+        btnAgrandi.classList.add("btnAgrandi");
+        tousLesBoutons.appendChild(btnAgrandi);
+        btnAgrandi.innerHTML = "<i class='fas fa-expand-alt'></i>";
+
+        let btnEdit = document.createElement('button');
+        btnEdit.classList.add("btnEdit");
+        tousLesBoutons.appendChild(btnEdit);
+        btnEdit.innerHTML = "<i class='fas fa-edit'></i>";
+
+        monPostIt.addEventListener("click",()=> {
+            numPostIt = this.num;
+            console.log(numPostIt);
+        })
     }
 }
--- js/index.js
+++ js/index.js
@@ -1,3 +1,26 @@
+let conteneur = document.querySelector('.conteneur');
+let btnAjoutPostIt = document.querySelector("#btnAjoutPostIt");
+let tabPostIt = [];
+
+let numPostIt;
+
+function createPostIt(){
+    conteneur.innerHTML = "";
+    tabPostIt.push(new PostIt(tabPostIt.length, 6, 6, "lightblue", "Ceci est un post-it !", 150, 150));
+    for(let i in tabPostIt){
+        tabPostIt[i].affiche();
+    }
+}
+
+// let postIt1 = new PostIt(5, 5, "lightgreen", "Ceci est mon premier post-it", 200, 200);
+// let postIt2 = new PostIt(5, 5, "lightblue", "Ceci est mon deuxième post-it, dans celui-ci j'aimerais écrire un de mes projets", 250, 250);
+
 window.addEventListener('load', () => {
-    
+    btnAjoutPostIt.addEventListener("click", ()=>{
+        createPostIt();
+        conteneur.appendChild(btnAjoutPostIt);
+    })
+    document.addEventListener("mousemove", e=>{
+        
+    })
 });
